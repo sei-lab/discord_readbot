6f47e59439c4c9c208c75d5f2e9df3be113a1882
{username} <{email}>
2026-07-30 17:34:06 +0900

    modified:   Nyarlathotep/src/discord_dicebot.py

--- Nyarlathotep/src/discord_dicebot.py
+++ Nyarlathotep/src/discord_dicebot.py
@@ -9,10 +9,38 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
+def judgement(result, target):
+    if result <= 5:
+        return "クリティカル"
+    elif result <= target / 5:
+        return "エクストリーム成功"
+    elif result < target / 2:
+        return "ハード成功"
+    elif result <= target:
+        return "成功"
+    elif result >= 95:
+        return "ファンブル"
+    else:
+        return "失敗"
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
+    if message.content.startswith('dd'):
+        # Extract the number of sides from the command
+        try:
+            target = int(message.content.split()[1])
+
+            result = random.randint(1,100)
+            judge = judgement(result, target)
+            await message.channel.send(f'1d100 --> {result} ({judge})')
+        except (IndexError, ValueError):
+            await message.channel.send("使い方: dd num")
 
 client.run(TOKEN)
