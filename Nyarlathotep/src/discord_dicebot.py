b1e93e4a53b0f7caa3ce975674e1bda07d1b3b73
{username} <{email}>
2026-08-11 18:07:15 +0900

    modified:   Nyarlathotep/src/discord_dicebot.py

--- Nyarlathotep/src/discord_dicebot.py
+++ Nyarlathotep/src/discord_dicebot.py
@@ -8,9 +8,9 @@ import re
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+DATA_PATH = os.getenv("DATA_PATH")
 
-skill_file_path = "/home/seifu/dev/python/projects/discord/Nyarlathotep/data/dice_data.json"
-with open(skill_file_path, "r", encoding="utf-8") as f:
+with open(DATA_PATH, "r", encoding="utf-8") as f:
     data = json.load(f)
 
 skill_list = data.get("skills", [])
@@ -41,7 +41,7 @@ def judgement(result, target):
     else:
         return "失敗"
 
-def create_message_content(author, num_dice, num_sides, rolls, total, result, judge=None, operator=None, modifier=None ,skill=None):
+def create_message_content(author, num_dice, num_sides, rolls, result, target=None, judge=None, operator=None, modifier=None ,skill=None):
     expr = f"{author}"
 
     if skill is not None:
@@ -52,10 +52,13 @@ def create_message_content(author, num_dice, num_sides, rolls, total, result, ju
     if operator:
         expr += f"\\{operator}{modifier}"
 
+    if target is not None:
+        expr += f" <= {target}"
+
     if num_dice == 1:
         detail = str(rolls[0])
     else:
-        detail = f"{rolls} = {total}"
+        detail = f"{rolls} = {sum(rolls)}"
 
     if operator:
         detail += f"\\{operator}{modifier} \n--> {result}"
@@ -64,16 +67,41 @@ def create_message_content(author, num_dice, num_sides, rolls, total, result, ju
         detail += f" ({judge})"
     return f"{expr}\n--> {detail}"
 
-def chack_next_word(word):
+def check_next_word(word):
     skill = None
     target = None
 
     if word.isdigit() or (word.startswith('-') and word[1:].isdigit()):
         target = int(word)
     elif word in skill_list:
-        skill = word
+        # skill = word
+        skill = None
 
     return skill, target
+
+def parse_dd(text, i):
+    if text[i].lower() == "dd":
+        if i + 1 < len(text):
+            return check_next_word(text[i + 1])
+        return None, None
+
+    m = re.fullmatch(r"dd(-?\d+)", text[i])
+    if m:
+        return None, int(m.group(1))
+
+    return None, None
+
+def parse_sdd(text, i):
+    if text[i].lower() == "sdd":
+        if i + 1 < len(text):
+            return check_next_word(text[i + 1])
+        return None, None
+
+    m = re.fullmatch(r"sdd(-?\d+)", text[i])
+    if m:
+        return None, int(m.group(1))
+
+    return None, None
 
 @client.event
 async def on_ready():
@@ -84,115 +112,77 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.author in blacklists:
+    if message.author.id in blacklists:
         return
 
     if message.content.startswith('よぐぱんち'):
         author = message.author.mention
         num_dice, num_sides = 1, 3
         rolls, result = roll_dice(num_dice, num_sides)
-        message_content = create_message_content(author, num_dice, num_sides, rolls, sum(rolls), result)
+        message_content = create_message_content(
+            author, 
+            num_dice, 
+            num_sides, 
+            rolls, 
+            sum(rolls), 
+            skill="よぐぱんち")
         await message.channel.send(message_content)
 
     if re.search(r"精神分析",message.content) and str(message.author.id) == "1529660407525019799":
         author = message.author.mention
         skill = "精神分析"
-        rolls, result = roll_dice(1, 100)
-        judge = judgement(result, 95)
-        message_content = create_message_content(author, num_dice, num_sides, rolls, sum(rolls), result, judge, skill=skill)
+        num_dice = 1
+        num_sides = 100
+        target = 95
+        rolls, result = roll_dice(num_dice, num_sides)
+        judge = judgement(result, target)
+        message_content = create_message_content(author, num_dice, num_sides, rolls, sum(rolls), target, judge, skill=skill)
         await message.channel.send(message_content)
 
     text = message.content.split()
     for i, t in enumerate(text):
-        if t.lower() == "dd":
-            rolls, result = roll_dice(1, 100)
-            if i + 1 < len(text):
-                skill ,target = chack_next_word(text[i+1])
-                if target is not None:
-                    judge = judgement(result, target)
-                    message_content = create_message_content(message.author.mention, 1, 100, rolls, sum(rolls), result, judge, skill=skill)
-                else:
-                    message_content = create_message_content(message.author.mention, 1, 100, rolls, sum(rolls), result, skill=skill)
-                message_content = create_message_content(message.author.mention, 1, 100, rolls, sum(rolls), result, judge)
-            else:
-                message_content = create_message_content(message.author.mention, 1, 100, rolls, sum(rolls), result, None)
+        skill, target = parse_dd(text, i)
+        if skill is not None or target is not None or t.lower() == "dd":
+            num_dice, num_sides = 1, 100
+            rolls, result = roll_dice(num_dice, num_sides)
+
+            judge = None
+            if target is not None:
+                judge = judgement(result, target)
+
+            message_content = create_message_content(
+                message.author.mention,
+                num_dice,
+                num_sides,
+                rolls,
+                result,
+                target,
+                judge,
+                skill=skill
+            )
+
             await message.channel.send(message_content)
-        m = re.fullmatch(r"dd(\d+)",t)
-        if m and (m.group(1).isdigit() or (m.group(1).startswith('-') and m.group(1)[1:].isdigit())):
-            target = int(m.group(1))
-            rolls, result = roll_dice(1, 100)
-            judge = judgement(result, target)
-            message_content = create_message_content(message.author.mention, 1, 100, rolls, sum(rolls), result, judge)
-            await message.channel.send(message_content)
-        
-        m = re.fullmatch(r"(\d+)[dD](\d+)(?:([+\-*/^])(\d+))?", t)
-        if m:
-            num_dice = int(m.group(1))
-            num_sides = int(m.group(2))
-            operator = m.group(3)
-            modifier = int(m.group(4)) if m.group(4) else None
+        skill, target = parse_sdd(text, i)
+        if skill is not None or target is not None or t.lower() == "sdd":
+            num_dice, num_sides = 1, 100
+            rolls, result = roll_dice(num_dice, num_sides)
 
-            if num_dice <= 0 or num_sides <= 0:
-                await message.channel.send(f"{message.author.mention} そのダイスは振れないよ(^^)")
-                return
-            elif num_dice * num_sides > 1000000000 or (operator == "^" and modifier and num_dice * num_sides * modifier > 1000000000):
-                await message.channel.send(f"{message.author.mention} ちょちょちょ多すぎるって...(^^)")
-                return
-            elif operator == "/" and modifier == 0:
-                await message.channel.send("0では割れないよ(^^)")
-                return
-            digits = len(str(num_sides))
-            estimated = num_dice * (digits + 2) + 100  # メンションなどの余裕
-            if estimated > 1900:
-                await message.channel.send(
-                f"{message.author.mention} 出力が長すぎるよ...(^^)"
-                )
-                return
+            judge = None
+            if target is not None:
+                judge = judgement(result, target)
 
-            # 通常のダイス
-            if operator != "^":
-                rolls, total = roll_dice(num_dice, num_sides)
+            message_content = create_message_content(
+                message.author.mention,
+                num_dice,
+                num_sides,
+                rolls,
+                result,
+                target,
+                judge,
+                skill=skill
+            )
 
-                if operator == "+":
-                    total += modifier
-                elif operator == "-":
-                    total -= modifier
-                elif operator == "*":
-                    total *= modifier
-                elif operator == "/":
-                    total //= modifier
-
-                expr = f"{num_dice}d{num_sides}"
-                if operator:
-                    expr += f"\\{operator}{modifier}"
-
-                if num_dice == 1:
-                    detail = str(rolls[0])
-                else:
-                    detail = f"{rolls} = {sum(rolls)}"
-
-                if operator:
-                    detail += f"\\{operator}{modifier} \n--> {total}"
-
-                await message.channel.send(
-                    f"{message.author.mention} {expr}\n--> {detail}"
-                )
-
-            # ^ の特殊処理
-            else:
-                totals = []
-
-                for _ in range(modifier):
-                    _, total = roll_dice(num_dice, num_sides)
-                    totals.append(total)
-
-                result = math.prod(totals)
-
-                await message.channel.send(
-                    f"{message.author.mention} "
-                    f"{num_dice}d{num_sides}^{modifier}\n"
-                    f"--> {totals}^{modifier}\n"
-                    f"--> {result}"
-                )
-
+            await message.delete()
+            await message.channel.send("🎲secret dice(^^)🎲")
+            await message.author.send(message_content)
 client.run(TOKEN)
